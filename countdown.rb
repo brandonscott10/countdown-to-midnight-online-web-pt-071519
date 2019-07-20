08260142705b6c612522b374c4fdb4fb3e57125c
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-20 22:48:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
 
 countdown = 10 
-while countdown <= 10 do 
+
   puts "#{countdown} SECOND(S)!"
   countdown -= 1 
 if countdown == 0
